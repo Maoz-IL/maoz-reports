b66ef0ae2ec046b5a4a66eadd8010d58d9a597f4
Lior Yitzhak <dev@lior.world>
2026-02-11 17:08:20 +0200

    Add previous button's styles & functionality

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -4,6 +4,8 @@ const titleStageIndicator = document.querySelector(
 );
 const titleStageIndicatorItem = titleStageIndicator.querySelector('li');
 const footerStageIndicator = [...document.querySelectorAll('.footer-stage-indicator li')];
+
+const btnPrev = document.querySelector('.footer-button-prev');
 const btnNext = document.querySelector('.footer-button-next');
 
 let currentStep = formSteps.findIndex((step) => step.classList.contains('current-step'));
@@ -16,5 +18,34 @@ btnNext.addEventListener('click', () => {
     formSteps[currentStep].classList.add('current-step');
     footerStageIndicator[currentStep].classList.add('current-step');
     titleStageIndicator.style.transform = `translateY(-${titleStageIndicatorItem.getBoundingClientRect().height * currentStep}px)`;
+
+    if (currentStep >= 1) {
+      btnNext.style.width = 'calc(85% - 0.5rem)';
+    }
+  }
+});
+
+btnPrev.addEventListener('click', () => {
+  if (currentStep > 0) {
+    // הסר מצב נוכחי מהשלב הנוכחי
+    formSteps[currentStep].classList.remove('current-step');
+    footerStageIndicator[currentStep].classList.remove('current-step');
+
+    // חזור שלב אחד אחורה
+    currentStep -= 1;
+
+    // הוסף מצב נוכחי לשלב החדש
+    formSteps[currentStep].classList.add('current-step');
+    footerStageIndicator[currentStep].classList.add('current-step');
+
+    // עדכן את תזוזת האינדיקטור
+    titleStageIndicator.style.transform = `translateY(-${titleStageIndicatorItem.getBoundingClientRect().height * currentStep}px)`;
+
+    // עדכן רוחב הכפתור "הבא" (כשתחזור ל-0 תחזיר לרוחב המקורי)
+    if (currentStep === 0) {
+      btnNext.style.width = ''; // חוזר ל-CSS
+    } else {
+      btnNext.style.width = 'calc(85% - 0.5rem)';
+    }
   }
 });
