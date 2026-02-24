6e806c4787d8713a004cf1af408171d9228a6e46
Lior Yitzhak <dev@lior.world>
2026-02-25 01:42:18 +0200

    Add data directory & workers dataset

---
+++ assets/js/data/workers.js
@@ -0,0 +1,23 @@
+export const workers = [
+  {
+    value: 'יוחאי מעוז',
+    label: 'יוחאי מעוז',
+    role: 'ראש צוות',
+    img: '/assets/images/profile-yohai-xs.avif',
+    alt: 'תמונת הפרופיל של העובד',
+  },
+  {
+    value: 'אשר נפתלי',
+    label: 'אשר נפתלי',
+    role: 'ראש צוות',
+    img: '/assets/images/profile-yohai-xs.avif',
+    alt: 'תמונת הפרופיל של העובד',
+  },
+  {
+    value: 'יוחאי מעוז',
+    label: 'יוחאי מעוז',
+    role: 'ראש צוות',
+    img: '/assets/images/profile-yohai-xs.avif',
+    alt: 'תמונת הפרופיל של העובד',
+  },
+];
