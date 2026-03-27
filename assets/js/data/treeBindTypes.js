cb76768b975071044610a301f2d00621b3369ec9
Lior Yitzhak <dev@lior.world>
2026-03-28 02:53:43 +0300

    Add treeBindTypes dataset

---
+++ assets/js/data/treeBindTypes.js
@@ -0,0 +1,14 @@
+export const treeBindTypes = [
+  {
+    value: '1 טון',
+    label: '1 טון',
+  },
+  {
+    value: '2 טון',
+    label: '2 טון',
+  },
+  {
+    value: '4 טון',
+    label: '4 טון',
+  },
+];
