06e74fdcab78082aa47a10fe82d1f8e9d21b73f2
Lior Yitzhak <dev@lior.world>
2026-04-29 14:33:29 +0300

    Add: fireberry schema (data mapping)

---
+++ assets/js/fireberry.schema.js
@@ -0,0 +1,37 @@
+// Fireberry Table Identifier
+export const FB_OBJECT = 'customobject1005';
+
+// Fireberry Data Types Mapping
+export const FB_TYPES = {
+  DATE: '83bf530c-e04c-462b-9ffc-a46f750fc072',
+  TEXT: 'a1e7ed6f-5083-477b-b44c-9943a6181359',
+  NUMBER: '6a34bfe3-fece-4da1-9136-a7b1e5ae3319',
+  FILE: 'ed2ad39d-32fc-4585-8f5b-2e93463f050a',
+  USER: 'a8fcdf65-91bc-46fd-82f6-1234758345a1',
+  DATETIME: 'ce972d02-5013-46d4-9d1d-f09df1ac346a',
+};
+
+// Fireberry Fields Mapping
+export const FB_FIELDS = {
+  date: 'pcfsystemfield1007',
+  teamLead: 'name',
+  teamMembers: 'pcfsystemfield1009',
+  vehicles: 'pcfsystemfield1010',
+  customer: 'pcfsystemfield1013',
+  address: 'pcfsystemfield1012',
+  taskStatus: 'pcfsystemfield1015',
+  taskStatusReasons: 'pcfsystemfield1016',
+  taskStatusDetails: 'pcfsystemfield1017',
+  workType: 'pcfsystemfield1018',
+  treeType: 'pcfsystemfield1020',
+  treesCount: 'pcfsystemfield1021',
+  treeHeight: 'pcfsystemfield1022',
+  treeBindType: 'pcfsystemfield1035',
+  treeBindLength: 'pcfsystemfield1025',
+  workHourDescription: 'pcfsystemfield1019',
+  transportsCount: 'pcfsystemfield1024',
+  treeStumpForCharge: 'pcfsystemfield1028',
+  workTypeDetails: 'pcfsystemfield1031',
+  workTypeNotes: 'pcfsystemfield1033',
+  photos: 'pcfsystemfield1034',
+};
